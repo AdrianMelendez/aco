352d8d0f0e3b6494ae84cb43e54558563c4eaa77
Adrian Melendez <adrianmelendezlorenzo@gmail.com>
2023-03-25 12:40:04 +0100

    Calculate pi: Threading

---
+++ calculate_pi_threaded.c
@@ -0,0 +1,14 @@
+// gcc -o p calculate_pi.c -lm 
+#include <stdio.h>
+#include <math.h>
+
+void main(){
+    double sum=0.;
+    int k=0;
+
+    for (k=0; k<100; k++){
+        sum = sum + pow((-1./3.), k) * 1./(2*k+1);
+    }
+    sum = sqrt(12)*sum;
+    printf("The result for pi is: %E \n", sum);
+}
