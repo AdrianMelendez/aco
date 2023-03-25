96bd107ec30ca93909457090941dfc43add9232e
Adrian Melendez <adrianmelendezlorenzo@gmail.com>
2023-03-25 12:34:59 +0100

    Calculation of pi: Without threading

--- calculate_pi.c
+++ calculate_pi.c
@@ -6,9 +6,9 @@ void main(){
     double sum=0.;
     int k=0;
 
-    for (k=0; k=10; k++){
+    for (k=0; k<100; k++){
         sum = sum + pow((-1./3.), k) * 1./(2*k+1);
     }
     sum = sqrt(12)*sum;
-    printf("The result for pi is: %E", sum);
+    printf("The result for pi is: %E \n", sum);
 }
