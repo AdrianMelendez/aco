a879829df956351cb08e2ce25610169ad14782c6
Adrian Melendez <adrianmelendezlorenzo@gmail.com>
2023-03-26 11:29:06 +0200

    Parallellised

--- calculate_pi_threaded.c
+++ calculate_pi_threaded.c
@@ -9,8 +9,13 @@ void main(){
     double sum=0.;
     int k=0;
 
-    for (k=0; k<100; k++){
-        sum = sum + pow((-1./3.), k) * 1./(2*k+1);
+    #pragma omp parallel shared(sum) 
+    {
+
+    #pragma omp for reduction(+:sum) 
+        for (k=0; k<100; k++){
+            sum = sum + pow((-1./3.), k) * 1./(2*k+1);
+        }
     }
     sum = sqrt(12)*sum;
     printf("The result for pi is: %E \n", sum);
